d289d776b12f450d310f865933fb09d3b6120a7d
romain-yohan julienne <romain.yohan@live.fr>
2019-01-30 15:22:33 +0100

    correction approximative des requêtes SQL

--- Models.Sql/Sql.cs
+++ Models.Sql/Sql.cs
@@ -14,14 +14,14 @@ namespace MyAirport.Pim.Models
         private string strCnx = ConfigurationManager.ConnectionStrings["MyAirport.Pim.Settings.DbConnect"].ConnectionString;
 
         private string commandGetBagageIata = "SELECT b.ID_BAGAGE, b.CODE_IATA, b.COMPAGNIE, b.LIGNE, b.DATE_CREATION, b.ESCALE, b.CLASSE," +
-            "b EN_CONTINUATION, cast(iif(bp.PARTICULARITE is null, 0, 1) as bit) as 'RUSH', cast(iif(b.EN_CONTINUATION like 'N', 0, 1) as bit ) as 'CONT', " +
+            "b.EN_CONTINUATION, cast(iif(bp.ID_PARTICULARITE is null, 0, 1) as bit) as 'RUSH', cast(iif(b.EN_CONTINUATION like 'N', 0, 1) as bit ) as 'CONT', " +
             "cast(iif(b.PRIORITAIRE like '0', 0, 1) as bit) as 'PRIO' " +
-            "FROM BAGAGE b left outer join BAGAGE_A_POUR_PARTICULARITE bp on bp.ID_BAGAGE and (bp.ID_PARTICULARITE = 15) where b.code_iata = @code_iata";
+            "FROM BAGAGE b left outer join BAGAGE_A_POUR_PARTICULARITE bp on (bp.ID_BAGAGE > 0 and bp.ID_PARTICULARITE = 15) where b.code_iata = @code_iata";
 
         private string commandGetBagageId = "SELECT b.ID_BAGAGE, b.CODE_IATA, b.COMPAGNIE, b.LIGNE, b.DATE_CREATION, b.ESCALE, b.CLASSE," +
-            "b EN_CONTINUATION, cast(iif(bp.PARTICULARITE is null, 0, 1) as bit) as 'RUSH', cast(iif(b.EN_CONTINUATION like 'N', 0, 1) as bit ) as 'CONT', " +
+            "b EN_CONTINUATION, cast(iif(bp.ID_PARTICULARITE is null, 0, 1) as bit) as 'RUSH', cast(iif(b.EN_CONTINUATION like 'N', 0, 1) as bit ) as 'CONT', " +
             "cast(iif(b.PRIORITAIRE like '0', 0, 1) as bit) as 'PRIO' " +
-            "FROM BAGAGE b left outer join BAGAGE_A_POUR_PARTICULARITE bp on bp.ID_BAGAGE and (bp.ID_PARTICULARITE = 15) where b.ID_BAGAGE = @id";
+            "FROM BAGAGE b left outer join BAGAGE_A_POUR_PARTICULARITE bp on (bp.ID_BAGAGE > 0 and bp.ID_PARTICULARITE = 15) where b.ID_BAGAGE = @id";
 
         private List<BagageDefinition> listBagage;
         public override BagageDefinition GetBagage(int idBagage)
