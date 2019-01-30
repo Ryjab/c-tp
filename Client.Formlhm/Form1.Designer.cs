3194046c4d93e23af0b2b880ee3b3f3a0bb240aa
romain-yohan julienne <romain-yohan.julienne@epita.fr>
2019-01-30 13:54:34 +0100

    Ajoutez des fichiers projet.

---
+++ Client.Formlhm/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Client.Formlhm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3126516156514",
+            "516516565165156"});
+            this.comboBox1.Location = new System.Drawing.Point(70, 150);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(418, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Models.Sql/Sql.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyAirport.Pim.Entities;
+
+namespace MyAirport.Pim.Models
+{
+    public class Sql : AbstractDefinition
+    {
+        private string strCnx = ConfigurationManager.ConnectionStrings["MyAirport.Pim.Settings.DbConnect"].ConnectionString;
+
+        private string commandGetBagageIata = "SELECT b.ID_BAGAGE, b.CODE_IATA, b.COMPAGNIE, b.LIGNE, b.DATE_CREATION, b.ESCALE, b.CLASSE," +
+            "b EN_CONTINUATION, cast(iif(bp.PARTICULARITE is null, 0, 1) as bit) as 'RUSH', cast(iif(b.EN_CONTINUATION like 'N', 0, 1) as bit ) as 'CONT', " +
+            "cast(iif(b.PRIORITAIRE like '0', 0, 1) as bit) as 'PRIO' " +
+            "FROM BAGAGE b left outer join BAGAGE_A_POUR_PARTICULARITE bp on bp.ID_BAGAGE and (bp.ID_PARTICULARITE = 15) where b.code_iata = @code_iata";
+
+        private string commandGetBagageId = "SELECT b.ID_BAGAGE, b.CODE_IATA, b.COMPAGNIE, b.LIGNE, b.DATE_CREATION, b.ESCALE, b.CLASSE," +
+            "b EN_CONTINUATION, cast(iif(bp.PARTICULARITE is null, 0, 1) as bit) as 'RUSH', cast(iif(b.EN_CONTINUATION like 'N', 0, 1) as bit ) as 'CONT', " +
+            "cast(iif(b.PRIORITAIRE like '0', 0, 1) as bit) as 'PRIO' " +
+            "FROM BAGAGE b left outer join BAGAGE_A_POUR_PARTICULARITE bp on bp.ID_BAGAGE and (bp.ID_PARTICULARITE = 15) where b.ID_BAGAGE = @id";
+
+        private List<BagageDefinition> listBagage;
+        public override BagageDefinition GetBagage(int idBagage)
+        {
+            BagageDefinition bagRes = null;
+            using (SqlConnection cnx = new SqlConnection(strCnx))
+            {
+                SqlCommand cmd = new SqlCommand(this.commandGetBagageId, cnx);
+                cmd.Parameters.AddWithValue("@id", idBagage);
+                cnx.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if(sdr.Read())
+                {
+                    bagRes = new BagageDefinition()
+                    {
+                        IdBagage = sdr.GetInt32(sdr.GetOrdinal("ID_BAGAGE")),
+                        CodeIata = sdr.GetString(sdr.GetOrdinal("CODE_IATA")),
+                        EnContinuation = sdr.GetBoolean(sdr.GetOrdinal("CONTINUATION")),
+                        Compagnie = sdr.GetString(sdr.GetOrdinal("COMPAGNIE"))
+                    };
+                }
+            }
+            return bagRes;
+        }
+
+        public override List<BagageDefinition> GetBagage(string codeIataBagage)
+        {
+            using (SqlConnection cnx = new SqlConnection(strCnx))
+            {
+                SqlCommand cmd = new SqlCommand(this.commandGetBagageIata, cnx);
+                cmd.Parameters.AddWithValue("@code_iata", codeIataBagage);
+                cnx.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                listBagage = new List<BagageDefinition>();
+                while (sdr.Read())
+                {
+                    listBagage.Add(new BagageDefinition()
+                    {
+                        IdBagage = sdr.GetInt32(sdr.GetOrdinal("ID_BAGAGE")),
+                        CodeIata = sdr.GetString(sdr.GetOrdinal("CODE_IATA")),
+                        EnContinuation = sdr.GetBoolean(sdr.GetOrdinal("CONTINUATION")),
+                        Compagnie = sdr.GetString(sdr.GetOrdinal("COMPAGNIE"))
+                    });
+                }
+            }
+            return listBagage;
+        }
+    }
+}
